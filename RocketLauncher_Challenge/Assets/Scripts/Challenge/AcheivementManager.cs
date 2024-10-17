74246767e3d4de69a42a953d1c2519ad47a5051a
minsu454 <kminsu454@gmail.com>
2024-10-17 13:09:24 +0900

    [Fix] Scroll View

--- RocketLauncher_Challenge/Assets/Scripts/Challenge/AcheivementManager.cs
+++ RocketLauncher_Challenge/Assets/Scripts/Challenge/AcheivementManager.cs
@@ -18,11 +18,12 @@ public class AchievementManager : MonoBehaviour
     private void Start()
     {
         achievementView.CreateAchievementSlots(achievements);  // UI 생성
+        RocketMovementC.OnHighScoreChanged += CheckAchievement;
     }
 
     // 최고 높이를 달성했을 때 업적 달성 판단, 이벤트 기반으로 설계할 것
     private void CheckAchievement(float height)
     {
-
+        achievementView.UnlockAchievement((int)height);
     }
 }
